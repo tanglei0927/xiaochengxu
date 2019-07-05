ff18f2f4fe944eb898c384bcd82bda10d21cfdc9
tanglei <1183722772@qq.com>
2019-07-05 14:44:40 +0800

    mpvue

---
+++ mpvue/src/pages/time/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="timepage">
+        <span>时间胶囊</span>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log("time")
+    }
+}
+</script>
+<style scoped>
+    .timepage{
+        height:100vh;
+        width:100%;
+        background-image: url("http://47.102.144.31/tang/images/bj1.jpg");
+        background-size: 100% 100%;
+
+    }
+</style>
+
+
